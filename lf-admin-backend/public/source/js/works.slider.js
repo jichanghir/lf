d82ce0f6bdc9709b71e421af966fde0cd4f96f34
alexserkin <a.serkyn@digitalcontact.com>
2018-01-27 23:05:40 +0200

    works slider prototype created

---
+++ lf-admin-backend/public/source/js/works.slider.js
@@ -0,0 +1,9 @@
+// $(function() {
+//     $('.works-slider__link-block').slick({
+//         dots: true,
+//         infinite: true,
+//         speed: 500,
+//         fade: true,
+//         cssEase: 'linear'
+//     });
+// });
